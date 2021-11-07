14913a27ca01ecc8c03b42440764da56c124b806
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2021-11-07 15:50:39 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -30,3 +30,33 @@ plot(l2011, col=cl)
 # 100 indicates how many tones we want to have in our palette
 
 plotRGB(l2011, r=3, g=3, b=1, stretch="Lin")
+
+# ---------- day 2
+# B1 is the reflectance in the blue band
+# B2 is the reflectance in the green band
+# B3 is the reflectance in the red band
+# B4 is the reflectance in the near infra-red band
+
+# Let's plot the green band
+plot(l2011$B2_sre)
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011$B2_sre, col=cl)
+
+# Change the colorRampPalette with dark green, green and light green
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+# Do the same for the blue band, that is called B1_sre
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+# Plot both images in just one multiframe graph (1 row and 2 columns)
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# 2 rows and 1 column
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
