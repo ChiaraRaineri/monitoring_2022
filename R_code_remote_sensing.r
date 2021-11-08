b0cef94e3e554f488e06450ea3bf295158960298
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2021-11-08 10:56:10 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -1,6 +1,7 @@
 # R code for ecosystem monitoring by remote sensing
 # First of all, we need to install additional packages
 # raster package to manage image data
+# All'inizio del codice devi sempre mettere library(raster), setwd("C/lab/") e l2011 <- brick("p224r63_2011.grd")
 
 install.packages("raster")
 
@@ -60,3 +61,39 @@ plot(l2011$B2_sre, col=clg)
 par(mfrow=c(2,1))
 plot(l2011$B1_sre, col=clb)
 plot(l2011$B2_sre, col=clg)
+
+
+# ---------- day 3
+
+# Plot the first four bands with 2 rows and 2 columns
+
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+
+clr <- colorRampPalette(c("dark red", "red", "pink"))(100)
+
+clnir <- colorRampPalette(c("red", "orange", "yellow"))(100)
+
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B3_sre, col=clr)
+plot(l2011$B4_sre, col=clnir)
+
+# Let's plot in RGB
+dev.off()
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")   # natural colors
+
+# Now let's see the NIR, shifting from B1, B2, B3 to B2, B3, B4 and other combinations
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")   # false colors
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")   # false colors
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")   # false colors
+
+par(mfrow=c(2,2))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
