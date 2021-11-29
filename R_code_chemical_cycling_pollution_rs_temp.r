834ede5752b4d7c2146ca9b127a5ea821c5fa8fe
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2021-11-29 09:55:26 +0100

    Update R_code_chemical_cycling_pollution_rs_temp.r

--- R_code_chemical_cycling_pollution_rs_temp.r
+++ R_code_chemical_cycling_pollution_rs_temp.r
@@ -1,1 +1,9 @@
 # Lesson 26/22/2021
+
+
+
+# Day 2
+
+# Importing all the data together with the lapply function
+# Writing "EN" means list all the files with the same name, i.e. EN
+rlist <- list.files(pattern="EN")
