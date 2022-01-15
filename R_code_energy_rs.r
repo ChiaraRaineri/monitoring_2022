1f07656436f67c1137671cb05dc70196877a5ae3
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2022-01-15 17:52:29 +0100

    Update R_code_energy_rs.r

--- R_code_energy_rs.r
+++ R_code_energy_rs.r
@@ -1,5 +1,6 @@
 # R code for estimating energy in ecosystems
 # How much energy is available for other organisms
+# I'll calculate the net primary production in 1992 and in 2006 to see the differences over that period of time
 
 # As I learned in the last lessons, first of all I need to recall the library raster, that allows me to see satellite images
 # install.packages("raster")
@@ -7,24 +8,33 @@ library(raster)
 # Then, another thing I can't forget is to set the working directory, in this case the lab folder
 setwd("C:/lab/")
 
-# To see the .jpg image we have to install another package
-install.packages("rgdal")
-library(rgdal)
+# The .jpg data I have are from Rio Peixoto (Brazil). They have been downloaded from NASA Earth Observatory
+# To see the .jpg image I have to install another package called gdal
+# gdal is a library for graphical data extraction. In R it is called rgdal
+install.packages("rgdal")  # I put the quotes because I am exiting R
+library(rgdal)  # Remember to recall it
+
+# To import the first image, the one from 1992, I have to use the brick() function (from the raster package)
+# but I also need the rgdal package
+l1992 <- brick("defor1_.jpg")  
+l1992  # Now I can see all the details from this image
+
+# In the image there are 3 bands: defor1_.1, defor1_.2, defor1_.3
 
 
-l1992 <- brick("defor1_.jpg")    # Image of 1992
-l1992
-
-# Bands: defor_.1, defor_.2, defor_.3
-
+# Let's make an RGB plot
 plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
 
-# defor1_.1 = NIR
+# defor1_.1 = NIR, because I put it in the red component and everything turned red (everything that reflect a lot in this band become red)
 # defor1_.2 = red
 # defor1_.3 = green
+# If I put the number 1 in g, it becomes green, and in b it becomes blue. With every color I can see different details
+# Usually water absorb all the NIR, so it becomes blue or black. In this case water is not black because the water is turbid (it looks like bare soil)
 
 
-# day 2
+
+# ---------- day 2
+
 
 l2006 <- brick("defor2_.jpg")
 
