644ca18a1d673ad51606e5776a64765703611d6a
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2022-01-18 12:41:08 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,7 +1,8 @@
 # R code for uploading and visualizing Copernicus data in R
 # https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
 
-# First, I should install the ncdf4 package
+# First, I should install the ncdf4 package to import the data
+# I am using it because the data from copernicus have .nc extension
 install.packages("ncdf4")
 
 library(raster)
@@ -14,44 +15,61 @@ setwd("C:/lab/copernicus")  # I created a new folder in the lab folder called co
 # The name of the object refers to the date of the data, so the 14th of December 2021
 snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
 snow20211214
+# The true name of the file is Snow.Cover.Extent
 
 # Let's plot the data directly
 plot(snow20211214)
 
 
 
+# ---------- day 2
 
+
+# Let's plot with a different palette
 cl <- colorRampPalette(c("dark blue","blue","light blue"))(100)
 plot(snow20211214, col=cl)
 
-# Bad color ramp palette (color blind people can't see the difference in colors)
+# This is an example of a bad color ramp palette (color blind people can't see the difference in colors)
 # https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
 cl <- colorRampPalette(c("blue","green","red"))(100)
 plot(snow20211214, col=cl)
 
+
+# viridis is a library from ggplot2
+# Using the viridis palette I can make sure that color blind people can see the image
+# There are several predefined palettes with different names
 install.packages("viridis")
 library(viridis)
 
 library(RStoolbox)
 library(ggplot2)
 
-ggplot() + geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent))
-# To know what to put in fill= you can recall snow20211214 and see "names"
 
+# Let's make a ggplot
+# Since I am plotting a raster file I should put geom_raster
+# To know what to put in fill= you can recall snow20211214 and see "names"
 ggplot() + 
-geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
-scale_fill_viridis()
+geom_raster(snow20211214, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) +
+scale_fill_viridis()  # The yellow is put in the maxima parts
+# If i don't put anything in scale_fill_viridis() it'll plot with the default palette, that is viridis
+# Let's assign it a title
 ggtitle("virids palette")
 
+# Let's plot with a different palette, in this case cividis
 ggplot() + 
-geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
-scale_fill_viridis(option="cividis")
+geom_raster(snow20211214, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) +
+scale_fill_viridis(option = "cividis")
 ggtitle("cividis palette")
 
-# viridis, cividis, magma, inferno, turbo, mako, plasma, rocket
+# These are the different color palettes that can be used: viridis, cividis, magma, inferno, turbo, mako, plasma, rocket
 
 
-#######
+
+# ---------- day 3
+
+
+# Now, I'll download another image from the same set (29th of August)
+
 
 rlist <- list.files(pattern="SCE")
 rlist
