33936e69b8745ca33b99c424dd77d2e0416e7aac
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2022-01-20 09:39:46 +0100

    Create R_code_sdm.r

---
+++ R_code_sdm.r
@@ -0,0 +1,5 @@
+# R code for species distribution modelling, namely the distribution of individuals of a population in space
+
+# Let's install a package for species distribution modelling
+# I am going to use data inside the package
+install.packages("sdm")
