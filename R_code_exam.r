1cd395214d3bbade94d8095a9a6e638dad2f6002
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2023-06-18 11:46:59 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1,10 +1,17 @@
 # Making sure that the packages are installed and recalling the libraries
 if (!require("ggplot2")) install.packages("ggplot2"); library("ggplot2")
+if (!require("viridis")) install.packages("viridis"); library("viridis")
 if (!require("raster")) install.packages("raster"); library("raster")
 if (!require("ncdf4")) install.packages("ncdf4"); library("ncdf4")
+if (!require("RStoolbox")) install.packages("RStoolbox"); library("RStoolbox")
+if (!require("gridExtra")) install.packages("gridExtra"); library("gridExtra")
+
+
 
 # Setting the working directory
 setwd("C:/lab/exam")
+
+
 
 
 
