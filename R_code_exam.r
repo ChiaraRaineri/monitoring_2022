ea35f5a1c97ee97945c479da66b170b7e01f1f64
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2023-06-18 11:21:30 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1,5 +1,11 @@
+# Making sure that the packages are installed and recalling the libraries
 if (!require("ggplot2")) install.packages("ggplot2"); library("ggplot2")
+if (!require("raster")) install.packages("raster"); library("raster")
 if (!require("ncdf4")) install.packages("ncdf4"); library("ncdf4")
+
+# Setting the working directory
+setwd("C:/lab/exam")
+
 
 
 
